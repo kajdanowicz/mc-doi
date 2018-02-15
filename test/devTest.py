6b6213cf45f4d60eceac2ba5ce0ad7adabc78c36
Tomasz Kajdanowicz <tomasz.kajdanowicz@pwr.edu.pl>
2018-02-15 13:10:30 +0100

    added events data loading

--- test/devTest.py
+++ test/devTest.py
@@ -1,13 +1,24 @@
 import model.parameters as model
-
+import data.Events as events
 
 def main():
-    cm = model.CorrelationMatrix(100)
-    cm.generateRandomCorrelationMatrix()
+    testLoadData()
+
+
+
+
+
+def testCreationOfRandomCorrelationMatrix(size=100):
+    cm = model.CorrelationMatrix(size)
+    print(cm.generateRandomCorrelationMatrix())
+
+def testSymetry(cm):
     print(cm.testSymetry())
+
+def testLoadData(fileName):
+    print(len(events(fileName)))
+
+
 
 if __name__ == "__main__":
     main()
-
-
-

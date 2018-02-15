fdd839867455fd2ac56d82eb185e1f2153629527
Tomasz Kajdanowicz <tomasz.kajdanowicz@pwr.edu.pl>
2018-02-15 13:42:28 +0100

    added init to events, get events

--- data/Events.py
+++ data/Events.py
@@ -2,8 +2,20 @@ import pandas as pd
 
 class EventsData():
 
-    def __init__(self, fileName):
-        self.data = self.loadCSV(fileName)
+    def __init__(self, fileName=None, dataFrame=None):
+        if dataFrame is not None:
+            self.data = dataFrame
+        elif fileName is not None:
+            self.data = self.loadCSV(fileName)
+        else:
+            self.data = pd.DataFrame()
 
     def loadCSV(self, fileName):
-        return pd.read_csv(fileName)
+        return EventsData(pd.read_csv(fileName))
+
+    def getEventsMinOccurences(self, minOccur):
+        ''' Return events that uses tags appearing in the data minOccur times.'''
+
+
+
+        return ()

1298faa06ba37349587501dc7d18805d4fa2b72d
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-02-21 17:56:44 +0100

    addContagionID contagionID attribute (boolean) contagionIDDict

--- data/data.py
+++ data/data.py
@@ -11,6 +11,8 @@ class data():
         self.numUsers = None
         self.numContagions = None
         self.numEvents = None
+        self.contagionID = False
+        self.contagionIDDict=None
 
     # def loadEventLogFile(self, filePath):
     #     self.eventLog=pd.read_csv(filePath)
@@ -155,3 +157,33 @@ class data():
         self.numEvents = self.eventLog.shape[0]
         self.numContagions = len(self.eventLog['contagion'].unique())
         # review
+
+    def addContagionID(self):
+        t = defaultdict(lambda: len(t))
+        self.eventLog['contagionID']=self.eventLog['contagion'].apply(lambda row: t[row['contagion']], axis=1)
+        self.contagionIDDict=t
+        # review
+
+    def estimateContagionCorrelationMatrix(self):
+        # TODO Implement
+        # self.size = eventsLog['tagID'].max()+1
+        # self.correlationMatrix = np.eye(N=self.size)
+        numUsers=self.numUsers
+        tmp = self.eventLog[['user', 'contagion']].drop_duplicates(subset=None, keep='first', inplace=False)
+        allUsersIDs = np.linspace(0, numUsers, num=numUsers + 1).astype(int)
+        for i in range(data.numContagions):
+            for j in range(i + 1, data.numContagions):
+                i_users = tmp[tmp['tagID'] == i]['user'].as_matrix()
+                j_users = tmp[tmp['tagID'] == j]['user'].as_matrix()
+                ni_users = np.setdiff1d(allUsersIDs, i_users)
+                nj_users = np.setdiff1d(allUsersIDs, j_users)
+                pij = len(np.intersect1d(i_users, j_users)) / numUsers
+                pinj = len(np.intersect1d(i_users, nj_users)) / numUsers
+                pnij = len(np.intersect1d(ni_users, j_users)) / numUsers
+                pi = len(i_users) / numUsers
+                pj = len(j_users) / numUsers
+                pni = len(ni_users) / numUsers
+                pnj = len(nj_users) / numUsers
+                wynik = pij / math.sqrt(pi * pj) - (pnij / math.sqrt(pni * pj) + pinj / math.sqrt(pi * pnj)) / 2
+                self.correlationMatrix[i][j] = wynik
+                self.correlationMatrix[j][i] = wynik
--- model/model.py
+++ model/model.py
@@ -11,33 +11,10 @@ class model():
         self.stateMatrix = None
         self.activityIndexVector = None
 
-    def estimateParameters(self,data):
+    def estimateParametersFromData(self,data):
         # TODO Implement this method
 
-    @staticmethod
-    def estimateContagionCorrelationMatrix(data):
-        # TODO Implement
-        # self.size = eventsLog['tagID'].max()+1
-        # self.correlationMatrix = np.eye(N=self.size)
-        numUsers=data.numUsers
-        tmp = data.eventsLog[['user', 'contagion']].drop_duplicates(subset=None, keep='first', inplace=False)
-        allUsersIDs = np.linspace(0, numUsers, num=numUsers + 1).astype(int)
-        for i in range(data.numContagions):
-            for j in range(i + 1, data.numContagions):
-                i_users = tmp[tmp['tagID'] == i]['user'].as_matrix()
-                j_users = tmp[tmp['tagID'] == j]['user'].as_matrix()
-                ni_users = np.setdiff1d(allUsersIDs, i_users)
-                nj_users = np.setdiff1d(allUsersIDs, j_users)
-                pij = len(np.intersect1d(i_users, j_users)) / numUsers
-                pinj = len(np.intersect1d(i_users, nj_users)) / numUsers
-                pnij = len(np.intersect1d(ni_users, j_users)) / numUsers
-                pi = len(i_users) / numUsers
-                pj = len(j_users) / numUsers
-                pni = len(ni_users) / numUsers
-                pnj = len(nj_users) / numUsers
-                wynik = pij / math.sqrt(pi * pj) - (pnij / math.sqrt(pni * pj) + pinj / math.sqrt(pi * pnj)) / 2
-                self.correlationMatrix[i][j] = wynik
-                self.correlationMatrix[j][i] = wynik
+
 
     @staticmethod
     def estimateAdajacencyMatrix(data):

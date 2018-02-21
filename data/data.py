bde4b5ecb6242f70301c4c9fd772215d61ced8c4
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-02-21 16:12:20 +0100

    deleteUsersAlterantive update

--- data/data.py
+++ data/data.py
@@ -140,13 +140,13 @@ class data():
 
     def deleteUsersAlterantive(self, userList):
         for user in userList:
-            self.edges.drop(self.edges[(self.edges['user1']==userList) | (self.edges['user2']==userList)].index,
+            self.edges.drop(self.edges[(self.edges['user1']==user) | (self.edges['user2']==user)].index,
                 inplace=True)
-            self.eventLog.drop(self.eventLog[self.eventLog['user']==userList].index, inplace=True)
+            self.eventLog.drop(self.eventLog[self.eventLog['user']==user].index, inplace=True)
             dict = {self.edges.values.max(): user}
-            self.edges['user1'].map(dict)
-            self.edges['user2'].map(dict)
-            self.eventLog['user'].map(dict)
+            self.edges['user1']=self.edges['user1'].map(dict)
+            self.edges['user2']=self.edges['user2'].map(dict)
+            self.eventLog['user']=self.eventLog['user'].map(dict)
         self.numUsers = len(np.union1d(self.edges['user1'], self.edges['user2']))
         self.numEvents = self.eventLog.shape[0]
         self.numContagions = len(self.eventLog['contagion'].unique())

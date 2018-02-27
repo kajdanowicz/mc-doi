7642b5633df2d5523f95099f6207791c2ec5111c
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-02-27 22:03:47 +0100

    tVector estimate init

--- model/tVector.py
+++ model/tVector.py
@@ -3,3 +3,13 @@ class tVector():
     def __init__(self):
         self.vector=None
         self.numUsers=None
+
+    def estimate(self,data):
+        #TODO Implement
+        indykatory_est = []
+        I = np.full((data.numUsers, data.numContagions), False, dtype=bool)
+        for i in range(history):
+            for index, row in event_log[event_log['ts'] == i].iterrows():
+                I[row['userNEW'], row['tagID']] = True
+            indykatory_est.append(I)
+            I = copy.deepcopy(I)

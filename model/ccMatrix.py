1937a7ade298bac8dd735252c8f3e4a3104cd9b5
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-02-27 21:51:42 +0100

    ccMatrix estimate

--- model/ccMatrix.py
+++ model/ccMatrix.py
@@ -1,4 +1,35 @@
+import numpy as np
+import math
+
+
 class ccMatrix():
+
     def __init__(self):
         self.matrix=None
         self.numContagions=None
+
+    def estimate(self,data):
+        # if data.contagionIDDict is None:
+        #     data.addContagionID()
+        self.numContagions=data.numContagions
+        self.matrix= np.eye(N=self.numContagions)
+        numUsers = data.numUsers
+        tmp = data.eventLog[['user', 'contagion']].drop_duplicates(subset=None, keep='first', inplace=False)
+        allUsersIDs = np.linspace(0, numUsers, num=numUsers + 1).astype(int)
+        for i in range(self.numContagions):
+            for j in range(i + 1, self.numContagions):
+                i_users = tmp[tmp['contagion'] == i]['user'].as_matrix()
+                j_users = tmp[tmp['contagion'] == j]['user'].as_matrix()
+                ni_users = np.setdiff1d(allUsersIDs, i_users)
+                nj_users = np.setdiff1d(allUsersIDs, j_users)
+                pij = len(np.intersect1d(i_users, j_users)) / numUsers
+                pinj = len(np.intersect1d(i_users, nj_users)) / numUsers
+                pnij = len(np.intersect1d(ni_users, j_users)) / numUsers
+                pi = len(i_users) / numUsers
+                pj = len(j_users) / numUsers
+                pni = len(ni_users) / numUsers
+                pnj = len(nj_users) / numUsers
+                wynik = pij / math.sqrt(pi * pj) - (pnij / math.sqrt(pni * pj) + pinj / math.sqrt(pi * pnj)) / 2
+                self.matrix[i][j] = wynik
+                self.matrix[j][i] = wynik
+        # review
--- model/model.py
+++ model/model.py
@@ -12,34 +12,10 @@ class model():
         self.stateMatrix = None # macierz indykatorow
         self.activityIndexVector = None # wykladnik
 
-    def estimateParametersFromData(self,data):
-        # TODO Implement this method
-        self.estimateContagionCorrelationMatrix(data)
+    def fit(self,data):
+        #TODO Implement this method
+        pass
 
-    def estimateContagionCorrelationMatrix(self,data):
-        if data.contagionIDDict is None:
-            data.addContagionID()
-        self.contagionCorrelationMatrix= np.eye(N=data.numContagions)
-        numUsers = data.numUsers
-        tmp = data.eventLog[['user', 'contagionID']].drop_duplicates(subset=None, keep='first', inplace=False)
-        allUsersIDs = np.linspace(0, numUsers, num=numUsers + 1).astype(int)
-        for i in range(data.numContagions):
-            for j in range(i + 1, data.numContagions):
-                i_users = tmp[tmp['contagionID'] == i]['user'].as_matrix()
-                j_users = tmp[tmp['contagionID'] == j]['user'].as_matrix()
-                ni_users = np.setdiff1d(allUsersIDs, i_users)
-                nj_users = np.setdiff1d(allUsersIDs, j_users)
-                pij = len(np.intersect1d(i_users, j_users)) / numUsers
-                pinj = len(np.intersect1d(i_users, nj_users)) / numUsers
-                pnij = len(np.intersect1d(ni_users, j_users)) / numUsers
-                pi = len(i_users) / numUsers
-                pj = len(j_users) / numUsers
-                pni = len(ni_users) / numUsers
-                pnj = len(nj_users) / numUsers
-                wynik = pij / math.sqrt(pi * pj) - (pnij / math.sqrt(pni * pj) + pinj / math.sqrt(pi * pnj)) / 2
-                self.contagionCorrelationMatrix[i][j] = wynik
-                self.contagionCorrelationMatrix[j][i] = wynik
-        # review
 
     def verifyContagionCorrelationMatrixSymetry(self):
         # TODO Implement "contagionCorrelationMatrix is None" exception

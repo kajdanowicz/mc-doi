39a4d406c59492a9ba86c573c0e472ec9450d126
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-03-03 23:47:25 +0100

    aMatrix estimate update

--- model/aMatrix.py
+++ model/aMatrix.py
@@ -8,33 +8,31 @@ class aMatrix():
     def estimate(self,data):
         # TODO Implement
         # TODO Analyse uniqueness of (user, action)
-        print('new')
         currentContagion=None
         a_u=defaultdict(lambda: 0)
         a_v2u = defaultdict(lambda: 0)
         a_vandu = defaultdict(lambda: 0)
         tau_vu = defaultdict(lambda: 0)
         data.addContagionID()
+        currentTable = []
         for index, row in data.eventLog.iterrows():
             if row['contagionID'] != currentContagion:
-                print(len(currentTable))
                 currentTable = []
                 currentContagion = row['contagionID']
             a_u[row['user']]+=1
             #parents=[]
             for tuple in currentTable:
-                if data.edgeExists(tuple(0),row['user']) & row['ts']>tuple(1):
-                    a_v2u[(tuple(0),row['user'])]+=1
-                    tau_vu[(tuple(0),row['user'])]+=(row['ts']-tuple(1))
+                if data.edgeExists(tuple[0],row['user']) & row['ts']>tuple[1]:
+                    a_v2u[(tuple[0],row['user'])]+=1
+                    tau_vu[(tuple[0],row['user'])]+=(row['ts']-tuple[1])
                     #parents.append(tuple(0))
-                a_vandu[(tuple(0),row['user'])]+=1
-                a_vandu[(row['user'],tuple(0))]+=1
+                a_vandu[(tuple[0],row['user'])]+=1
+                a_vandu[(row['user'],tuple[0])]+=1
             #for v in parents:
                 #update credit_v,u
             currentTable.append((row['user'],row['ts']))
         print(a_u)
         print(a_v2u)
-        print(a_vandu)
 
 
 

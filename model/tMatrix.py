f45d52183801c953ca44dd951a93cec21397a869
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-03-27 17:15:12 +0200

    tMatrix new version without pandas dataframe -> much faster

--- model/tMatrix.py
+++ model/tMatrix.py
@@ -33,38 +33,26 @@ class tMatrix():
         aMatrix.transpose()
         # print('aMatrix.matrixTransposed.shape', aMatrix.matrixTransposed.shape)
         # print('indicators[0].shape', indicators[0].shape)
+        max_neg = defaultdict(lambda : -2)
+        min_pos = defaultdict(lambda : 2)
         for l in trange(len(indicators) - 1):
             U = aMatrix.matrixTransposed.dot(indicators[l])
             F = U.dot(ccMAtrix.matrix) / data.numContagions
-            temp = np.logical_xor(indicators[l], indicators[l + 1])
-            temp1 = np.logical_or(temp, indicators[l])
+            temp = np.logical_xor(indicators[l], indicators[l + 1]) #aktywowane z l na l+1
+            temp1 = np.logical_or(temp, indicators[l]) #nieaktywowane z l na l+1 z wylaczeniem wczesniej aktywnych (po nalozeniu nagacji)
             activated = set()
             for i in range(data.numUsers):
                 for j in range(data.numContagions):
                     if temp[i][j]:
-                        if (F[i][j] > 0):
-                            df_thresholds.append([i, F[i][j], 1, Y[i]])
+                        if F[i][j] > 0:
+                            min_pos[i] = min(min_pos[i], 1 - math.pow(1 - F[i][j], 1 / float(Y[i] + 1)))
                         else:
-                            df_thresholds.append([i, 0, 1, Y[i]])
+                            min_pos[i] = min(min_pos[i], 0) #czy chcemy wyeliminować aktywacje, przy ujemnym wpływie?
                         activated.add(i)
                     if not temp1[i][j]:
-                        df_thresholds.append([i, F[i][j], 0, Y[i]])
+                        max_neg[i] = max(max_neg[i], 1 - math.pow(1 - F[i][j], 1 / float(Y[i] + 1)))
             for i in activated:
                 Y[i] += 1
-        df_thresholds = pd.DataFrame(df_thresholds)
-        df_thresholds.columns = ['user', 'x', 'y', 'w']
-        max_neg = defaultdict(lambda : -2)
-        min_pos = defaultdict(lambda : 2)
-        def test(row):
-            user = row.values[0]
-            x = row.values[1]
-            y = row.values[2]
-            w = row.values[3]
-            if y == 0:
-                max_neg[user] = max(max_neg[user], 1 - math.pow(1 - x, 1 / float(w + 1)))
-            else:
-                min_pos[user] = min(min_pos[user], 1 - math.pow(1 - x, 1 / float(w + 1)))
-        df_thresholds.progress_apply(test,axis=1)
         results = []
         for user in trange(data.numUsers):
             if min_pos[user] > 1:

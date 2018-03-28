8cc9aa1209b126496badba67bae102fd0b135769
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-03-28 10:30:00 +0200

    results init

--- model/results.py
+++ model/results.py
@@ -1,3 +1,17 @@
+import copy
+
 class results():
+
     def __init__(self):
-        pass
+        self.list = []
+        self.numResults = 0
+
+    def addResult(self,result):
+        self.list.append(copy.copy(result))
+        self.numResults += 1
+
+    def getResult(self,resultNum):
+        if resultNum >= self.numResults:
+            return False
+        else:
+            return self.list[resultNum]

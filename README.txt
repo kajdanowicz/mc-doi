ad81904c1a76ee5e3e51a5adee7897b19abc047b
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-04-24 17:13:22 +0200

    setup update MANIFEST.in added README.txt added

---
+++ README.txt
@@ -0,0 +1,4 @@
+# mc-doi: Multi contagion diffusion of information
+
+This library enables to proceed prediction of multi contagion diffusion processes of information
+
--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from distutils.core import setup
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 install_reqs = parse_requirements('requirements.txt', session='hack')
 reqs = [str(ir.req) for ir in install_reqs]
@@ -11,8 +14,8 @@ setup(
     url='http://github.com/kajdanowicz/mc-doi',
     license='Apache 2.0',
     author='Maciej Falkiewicz',
-    author_email='',
+    author_email='falkiewicz.maciej@gmail.com',
     description='',
-    install_requires=reqs
+#    install_requires=reqs
 )
 

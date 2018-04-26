f7097588ced32242a1111ffe88054b25d2dd0739
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-04-26 10:19:13 +0200

    setup.py update according to https://github.com/kennethreitz/samplemod

--- setup.py
+++ setup.py
@@ -1,21 +1,43 @@
-from distutils.core import setup
-try: # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError: # for pip <= 9.0.3
-    from pip.req import parse_requirements
+# from distutils.core import setup
+# try: # for pip >= 10
+#     from pip._internal.req import parse_requirements
+# except ImportError: # for pip <= 9.0.3
+#     from pip.req import parse_requirements
+#
+# install_reqs = parse_requirements('requirements.txt', session='hack')
+# reqs = [str(ir.req) for ir in install_reqs]
 
-install_reqs = parse_requirements('requirements.txt', session='hack')
-reqs = [str(ir.req) for ir in install_reqs]
+
+from setuptools import setup, find_packages
+
+# Package meta-data.
+NAME = 'mcdoi'
+DESCRIPTION = 'mcdoi package for Python 3'
+URL = 'http://github.com/kajdanowicz/mc-doi'
+EMAIL = 'falkiewicz.maciej@gmail.com'
+AUTHOR = 'Maciej Falkiewicz'
+REQUIRES_PYTHON = '>=3.5.0'
+VERSION = None
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
 
 setup(
-    name='mcdoi',
+    name=NAME,
     version='0.0.1',
-    packages=[''],
-    url='http://github.com/kajdanowicz/mc-doi',
-    license='Apache 2.0',
-    author='Maciej Falkiewicz',
-    author_email='falkiewicz.maciej@gmail.com',
-    description='',
+    description = DESCRIPTION,
+    long_description=readme,
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    license=license,
+    py_modules=['mc-doi'],
+    include_package_data=True,
 #    install_requires=reqs
 )
 

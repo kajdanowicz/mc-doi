65f0b6c89d3c54777719aa8d0a44a28fcecd2896
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-05-14 12:38:17 +0200

    adjacency refactred: -MLE_Beroulli_trial extracted

--- model/adjacency.py
+++ model/adjacency.py
@@ -11,22 +11,26 @@ class Adjacency():
         # TODO follow sklearn way
         self.matrix = None
 
-    def transpose(self):
+    def _transpose(self):
         if self.__dict__.get('matrix_transposed_',None) is None:
             self.matrix_transposed_ = self.matrix.transpose()
 
     def __clean_counters(self):
+        # TODO What if dicts do not exist?
         del self.v_2_u_
         del self.v_and_u_
         del self.u_
 
     def estimate(self, data: Data):
+        if data.sorted is False:
+            raise NameError('Data not sorted. Can not estimate adjacency matrix.')
         self.num_users_ = data.num_users
         self.v_2_u_ = defaultdict(lambda: 0)
         self.v_and_u_ = defaultdict(lambda: 0)
         self.u_ = defaultdict(lambda: 0)
         self.event_queue_ = dict()
         prev_contagion = None
+        # TODO implement whithout networkx library
         data.add_graph()
         d = data.event_log.drop_duplicates(subset=['contagion', 'user'], keep='first')
         for row in d.itertuples(index=False, name=None):
@@ -48,13 +52,17 @@ class Adjacency():
         nx.set_edge_attributes(G, 0, 'weight')
         for v in self.u_:
             for u in G.successors(v):
-                G[v][u]['weight'] = round(float(self.v_2_u_[(v, u)]) / float(self.u_[v]), 6)
+                # TODO Implement Jaccard index MLE
+                G[v][u]['weight'] = self.__MLE_Bernoulli_trial(u, v)
         for i in G.nodes():
             in_degree = G.in_degree(i, weight='weight')
             if in_degree != 0:
                 for v, u in G.in_edges(i):
                     G[v][u]['weight'] /= in_degree
         self.matrix = nx.adjacency_matrix(G, weight='weight')
+
+    def __MLE_Bernoulli_trial(self, u, v):
+        return round(float(self.v_2_u_[(v, u)]) / float(self.u_[v]), 6)
 
     def __reset_event_queue(self):
         self.event_queue_ = dict()

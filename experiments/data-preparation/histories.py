733bec3494e025961b3cd164556f68bf11b7fe3e
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-05-29 15:03:00 +0200

    histories - tqdm added

--- experiments/data-preparation/histories.py
+++ experiments/data-preparation/histories.py
@@ -18,11 +18,11 @@ end_time = 1335416399
 duration_24h_in_sec = 60*60*24
 time_grid = np.arange(start_time+duration_24h_in_sec,end_time+duration_24h_in_sec,duration_24h_in_sec)
 
-for dataset in tqdm(next(os.walk(directory))[1][:1]):
+for dataset in tqdm(next(os.walk(directory))[1]):
     set = directory + dataset
     event_log = pd.read_csv(set + '/event_log')
     event_log.columns = ['ts', 'user', 'contagion']
-    for counter, time_limit in enumerate(time_grid, 1):
+    for counter, time_limit in tqdm(enumerate(time_grid, 1)):
         file_name = set + '/history_' + str(counter) + '/event_log'
         os.makedirs(os.path.dirname(file_name), exist_ok=True)
         with open(file_name, 'w', encoding='utf-8') as handle:

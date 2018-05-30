9e298f29c8d55e1d83e37e100eebf47670da1d47
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-05-30 15:58:04 +0200

    verify_histories empty event_log protection

--- experiments/data-preparation/verify_histories.py
+++ experiments/data-preparation/verify_histories.py
@@ -20,12 +20,13 @@ duration_24h_in_sec = 60*60*24
 time_grid = np.arange(start_time+duration_24h_in_sec,end_time+duration_24h_in_sec,duration_24h_in_sec)
 
 for dataset in tqdm(next(os.walk(directory))[1]):
-    set = directory + dataset
-    edges = pd.read_csv(set + '/edges')
+    secik = directory + dataset
+    edges = pd.read_csv(secik + '/edges')
     edges.columns = ['user1','user2']
-    for history in tqdm(range(1,31)):
-        file_name = set + '/history_' + str(history)
-        event_log = pd.read_csv(file_name + '/event_log')
-        event_log.columns = ['ts', 'user', 'contagion']
-        if not set(event_log['user']).issubset(edges['user_1'].append(edges['user_2'])):
-            print(file_name)
+    for history in range(1,31):
+        file_name = secik + '/history_' + str(history)
+        if sum(1 for line in open(file_name + '/event_log', 'r', encoding='utf-8')) > 0:
+            event_log = pd.read_csv(file_name + '/event_log')
+            event_log.columns = ['ts', 'user', 'contagion']
+            if not set(event_log['user']).issubset(edges['user1'].append(edges['user2'])):
+                print(file_name)

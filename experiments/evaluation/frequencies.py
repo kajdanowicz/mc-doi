f24e1150546f31e7a5a78fa64ae50ffa86a7540a
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-06-15 10:01:12 +0200

    evaluation - frequencies init

---
+++ experiments/evaluation/frequencies.py
@@ -0,0 +1,48 @@
+import sys
+import os
+sys.path.append('/home/maciek/pyCharmProjects/mc-doi')
+from datetime import datetime
+import logging
+from model.multi_contagion_models import MultiContagionDynamicThresholdModel as MCDOI
+from data.data import Data
+from model.results import Results
+import pickle
+from tqdm import tqdm
+import numpy as np
+import pandas as pd
+from data.data import Data
+from model.parameters import ContagionCorrelation, Adjacency
+from copy import copy
+
+directory = '/datasets/mcdoi/louvain/'
+
+from joblib import Parallel, delayed
+import time
+def text_progessbar(seq, total=None):
+    step = 1
+    tick = time.time()
+    while True:
+        time_diff = time.time()-tick
+        avg_speed = time_diff/step
+        total_str = 'of %n' % total if total else ''
+        print('step', step, '%.2f' % time_diff, 'avg: %.2f iter/sec' % avg_speed, total_str)
+        step += 1
+        yield next(seq)
+all_bar_funcs = {
+    'txt': lambda args: lambda x: text_progessbar(x, **args),
+    'None': lambda args: iter,
+}
+def ParallelExecutor(use_bar='tqdm', **joblib_args):
+    def aprun(bar=use_bar, **tq_args):
+        def tmp(op_iter):
+            if str(bar) in all_bar_funcs.keys():
+                bar_func = all_bar_funcs[str(bar)](tq_args)
+            else:
+                raise ValueError("Value %s not supported as bar type"%bar)
+            return Parallel(**joblib_args)(bar_func(op_iter))
+        return tmp
+    return aprun
+
+aprun = ParallelExecutor(n_jobs=1)
+
+

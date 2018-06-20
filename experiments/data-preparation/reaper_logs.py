c98bf7650d5a114825cc14951d6eb776ddadf849
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-06-20 11:58:39 +0200

    reaper_logs init

---
+++ experiments/data-preparation/reaper_logs.py
@@ -0,0 +1,17 @@
+import sys
+import os
+sys.path.append('/home/maciek/pyCharmProjects/mc-doi')
+from datetime import datetime
+import logging
+from model.multi_contagion_models import MultiContagionDynamicThresholdModel as MCDOI
+from data.data import Data
+from model.results import Results
+import pickle
+from tqdm import tqdm
+import numpy as np
+import pandas as pd
+from data.data import Data
+from model.parameters import ContagionCorrelation, Adjacency
+from copy import copy
+
+file_to_repare = list(sys.argv)[1]

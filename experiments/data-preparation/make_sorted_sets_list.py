23be7256b8bdf9370438b842f1d7cea869557aa9
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-06-20 04:17:37 +0200

    cc+a-louvain - sort sets; write to file

--- experiments/data-preparation/make_sorted_sets_list.py
+++ experiments/data-preparation/make_sorted_sets_list.py
@@ -11,13 +11,13 @@ from tqdm import tqdm
 import numpy as np
 import pandas as pd
 from data.data import Data
+import operator
 
 directory = '/datasets/mcdoi/louvain/'
 
 d = dict()
 
-open(directory+'sorted_sets_list', 'w', encoding='utf-8').close()
-for dataset in tqdm(next(os.walk(directory))[1]):
+for dataset in next(os.walk(directory))[1]:
     secik = directory + dataset
     edges = pd.read_csv(secik + '/edges', header=None)
     edges.columns = ['user1','user2']
@@ -25,7 +25,11 @@ for dataset in tqdm(next(os.walk(directory))[1]):
         event_log = pd.read_csv(secik + '/event_log', header=None)
         event_log.columns = ['ts', 'user', 'contagion']
         set_id = secik.split('_')[-2] + '_' + secik.split('_')[-1]
-        print(set_id)
+        d[set_id] = len(event_log['contagion'].unique())
+sorted_tuples = sorted(d.items(), key=operator.itemgetter(1))
+for t in sorted_tuples:
+    with open(directory+'sorted_sets_list', 'a+', encoding='utf-8') as handle:
+        handle.write(directory +'/louvain_' + t[0] + '\n')
 
     #     if not set(event_log['user']).issubset(edges['user1'].append(edges['user2'])):
     #         with open(directory+'sets_to_omit', 'a', encoding='utf-8') as handle:

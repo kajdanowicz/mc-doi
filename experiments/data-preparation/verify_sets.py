1e968f32df001846dc737761f9de01b43588f818
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-06-20 03:04:54 +0200

    verify_sets - header = None

--- experiments/data-preparation/verify_sets.py
+++ experiments/data-preparation/verify_sets.py
@@ -17,10 +17,10 @@ directory = '/datasets/mcdoi/louvain/'
 open(directory+'sets_to_omit', 'w', encoding='utf-8').close()
 for dataset in tqdm(next(os.walk(directory))[1]):
     secik = directory + dataset
-    edges = pd.read_csv(secik + '/edges')
+    edges = pd.read_csv(secik + '/edges', header=None)
     edges.columns = ['user1','user2']
     if sum(1 for line in open(secik + '/event_log', 'r', encoding='utf-8')) > 0:
-        event_log = pd.read_csv(secik + '/event_log')
+        event_log = pd.read_csv(secik + '/event_log', header=None)
         event_log.columns = ['ts', 'user', 'contagion']
         if not set(event_log['user']).issubset(edges['user1'].append(edges['user2'])):
             with open(directory+'sets_to_omit', 'a', encoding='utf-8') as handle:

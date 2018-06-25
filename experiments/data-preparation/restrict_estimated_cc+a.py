02ed05b8a3c21221913b2fbd057c66a35204ae1e
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-06-25 10:40:25 +0200

    restrict_estimated_cc+a - final version

--- experiments/data-preparation/restrict_estimated_cc+a.py
+++ experiments/data-preparation/restrict_estimated_cc+a.py
@@ -11,5 +11,9 @@ with open(estimated_cc_a_file, 'r', encoding='utf-8') as file:
     estimated_cc_a = file.readlines()
 estimated_cc_a = [x.strip() for x in estimated_cc_a]
 
+num_users = 200
+
 for e in estimated_cc_a:
-    print(e.split('/')[3].split('_')[2])
+    if int(e.split('/')[4].split('_')[2])>=num_users:
+        with open(directory+'estimated_cc+a_num_users', 'a+', encoding='utf-8') as handle:
+            handle.write(e + '\n')

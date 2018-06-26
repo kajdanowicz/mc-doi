d4c02e917f2f67d4f9cc56afecec8f6df07c9026
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-06-26 19:37:20 +0200

    restricted_estimated_cc+a - path switched to /nfs/maciej/

--- experiments/data-preparation/restrict_estimated_cc+a.py
+++ experiments/data-preparation/restrict_estimated_cc+a.py
@@ -3,7 +3,7 @@ import os
 sys.path.append('/home/maciek/pyCharmProjects/mc-doi')
 import operator
 
-directory = '/datasets/mcdoi/louvain/'
+directory = '/nfs/maciej/mcdoi/louvain/'
 min_num_users = 200
 
 estimated_cc_a_file = directory + 'estimated_cc+a'
@@ -14,6 +14,6 @@ estimated_cc_a = [x.strip() for x in estimated_cc_a]
 num_users = 200
 
 for e in estimated_cc_a:
-    if int(e.split('/')[4].split('_')[2])>=num_users:
+    if int(e.split('/')[5].split('_')[2])>=num_users:
         with open(directory+'estimated_cc+a_num_users', 'a+', encoding='utf-8') as handle:
             handle.write(e + '\n')

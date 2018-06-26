bae2b2299db1913c38d2bf75104e2fc90ac896a0
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-06-26 15:19:27 +0200

    evaluation - histograms_frequencies - working version with subplots

--- experiments/evaluation/histograms_frequencies.py
+++ experiments/evaluation/histograms_frequencies.py
@@ -6,26 +6,42 @@ from tqdm import tqdm
 import numpy as np
 import pandas as pd
 import csv
+from matplotlib import pyplot as plt
 
-directory = '/datasets/mcdoi/louvain/'
+directory = '/nfs/maciej/louvain/'
 
-evaluated = set()
-for batch_size in [3600, 43200, 86400, 604800]:
-    with open(directory + 'frequencies/frequencies_'+str(batch_size), 'r', encoding='utf-8') as file:
-        e = file.readlines()
-    evaluated.update([x.strip() for x in e])
+def histogram(list_of_directories,batch_size):
+    abs_diffs = []
+    org = []
+    for d in list_of_directories:
+        for i in range(3):
+            if batch_size==604800:
+                if os.path.isfile(d+'/frequencies_'+str(i)):
+                    with open(d+'/frequencies_'+str(i), 'r', encoding='utf-8') as file:
+                        spamreader = csv.reader(file, delimiter=',', quotechar='"')
+                        for row in spamreader:
+                            abs_diffs.append(np.absolute(float(row[1])-float(row[2])))
+                            org.append(float(row[1]))
+            else:
+                with open(d + '/frequencies_' + str(i), 'r', encoding='utf-8') as file:
+                    spamreader = csv.reader(file, delimiter=',', quotechar='"')
+                    for row in spamreader:
+                        abs_diffs.append(np.absolute(float(row[1]) - float(row[2])))
+                        org.append(float(row[1]))
 
-def histogram(list_of_directories):
-    abd_diffs = []
-    for directory in list_of_directories:
-        with open(directory, 'r', encoding='utf-8', newline='') as file:
-            spamreader = csv.reader(file, delimiter=',', quotechar='|')
-            for row in spamreader:
-                print(row)
+    plt.figure()
+    plt.subplot(1,2,1)
+    plt.hist(abs_diffs, bins=50)
+    plt.title('Absolute differences')
+    plt.subplot(1,2,2)
+    plt.hist(org, bins=50)
+    plt.title('Frequencies from data')
+    plt.savefig(directory+'histograms/hist_'+str(batch_size)+'.png', dpi=300)
+
 
 if __name__ == '__main__':
-    for batch_size in [3600, 43200, 86400, 604800]:
+    for batch_size in tqdm([3600, 43200, 86400, 604800]):
         with open(directory + 'frequencies/frequencies_' + str(batch_size), 'r', encoding='utf-8') as file:
             e = file.readlines()
         evaluated = set([x.strip() for x in e])
-        histogram(evaluated)
+        histogram(evaluated,batch_size)

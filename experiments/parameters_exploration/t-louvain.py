10d65a4409e2f6645673b74fb380ae749609f676
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-06-26 19:48:09 +0200

    t-louvain - path switched to /nfs/maciej/

--- experiments/parameters_exploration/t-louvain.py
+++ experiments/parameters_exploration/t-louvain.py
@@ -43,13 +43,13 @@ def ParallelExecutor(use_bar='tqdm', **joblib_args):
 
 aprun = ParallelExecutor(n_jobs=6)
 
-directory = '/datasets/mcdoi/louvain/'
+directory = '/nfs/maciej/mcdoi/louvain/'
 
 with open(directory+'estimated_t+predict', 'r', encoding='utf-8') as file:
     estimated = file.readlines()
 estimated = [x.strip() for x in estimated]
 
-batch_sizes = [43200, 86400] # (1h), 12h, 24h, (7d)
+batch_sizes = [43200, 86400, 604800] # (1h), 12h, 24h, 7d
 batch_sizes.reverse()
 
 # with open(directory + 'sets_to_omit', 'r', encoding='utf-8') as sets_to_omit:

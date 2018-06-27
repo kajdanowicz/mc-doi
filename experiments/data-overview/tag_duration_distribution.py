94d2b327a0c03c9a3d36a0db7ccf4a10ad467ed8
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-06-27 22:28:11 +0200

    tag_duration_distribution - added ready histograms exclusion

--- experiments/data-overview/tag_duration_distribution.py
+++ experiments/data-overview/tag_duration_distribution.py
@@ -1,6 +1,7 @@
 import sys
 import pandas as pd
 from matplotlib import pyplot as plt
+import os
 
 directory = '/nfs/maciej/mcdoi/louvain/'
 
@@ -38,25 +39,31 @@ def ParallelExecutor(use_bar='tqdm', **joblib_args):
 
 aprun = ParallelExecutor(n_jobs=16)
 
-def tag_dist_for_set(set):
+ready_hists = set()
+for root, dirs, files in os.walk('/nfs/maciej/mcdoi/louvain/data-overview/', topdown=False):
+    for name in files:
+        ready_hists.add(os.path.join(root, name))
+
+def tag_dist_for_set(set, ready):
     num_users = set.split('/')[5].split('_')[2]
     set_id = set.split('/')[5].split('_')[1]
     for i in range(1,34):
         diff = []
-        if sum(1 for line in open(set + '/history_' + str(i) + '/event_log', 'r', encoding='utf-8')) > 0:
-            event_log = pd.read_csv(set + '/history_' + str(i) + '/event_log', header=None)
-            event_log.columns = ['ts', 'user', 'contagion']
-            tags_list = event_log['contagion'].unique()
-            for tag in tags_list:
-                d = event_log[event_log['contagion']==tag]['ts'].max()-event_log[event_log['contagion']==tag]['ts'].min()
-                if d>0:
-                    diff.append(d)
-            plt.figure()
-            plt.hist(diff, bins=50)
-            plt.title('Num of tags: '+ str(len(tags_list)) +', history ' + str(i))
-            plt.xlabel('Tag duration (last-first)')
-            plt.savefig(directory+'data-overview/'+num_users+'_'+set_id+'_hist_'+str(i)+'_duration_dist.png', dpi=72)
-            plt.close('all')
+        if directory+'data-overview/'+num_users+'_'+set_id+'_hist_'+str(i)+'_duration_dist.png' not in ready:
+            if sum(1 for line in open(set + '/history_' + str(i) + '/event_log', 'r', encoding='utf-8')) > 0:
+                event_log = pd.read_csv(set + '/history_' + str(i) + '/event_log', header=None)
+                event_log.columns = ['ts', 'user', 'contagion']
+                tags_list = event_log['contagion'].unique()
+                for tag in tags_list:
+                    d = event_log[event_log['contagion']==tag]['ts'].max()-event_log[event_log['contagion']==tag]['ts'].min()
+                    if d>0:
+                        diff.append(d)
+                plt.figure()
+                plt.hist(diff, bins=50)
+                plt.title('Num of tags: '+ str(len(tags_list)) +', history ' + str(i))
+                plt.xlabel('Tag duration (last-first)')
+                plt.savefig(directory+'data-overview/'+num_users+'_'+set_id+'_hist_'+str(i)+'_duration_dist.png', dpi=72)
+                plt.close('all')
 
 if __name__ == '__main__':
-    aprun(bar='txt')(delayed(tag_dist_for_set)(set) for set in sets_to_proceed)
+    aprun(bar='txt')(delayed(tag_dist_for_set)(set,ready_hists) for set in sets_to_proceed)

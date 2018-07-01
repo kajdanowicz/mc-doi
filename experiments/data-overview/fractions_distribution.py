849f9d90a07c4965ecd50be1e0723abcce528416
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-07-01 15:42:41 +0200

    data-overview - fractions_distribution - writing to file added

--- experiments/data-overview/fractions_distribution.py
+++ experiments/data-overview/fractions_distribution.py
@@ -22,6 +22,12 @@ for count, time in tqdm(enumerate(time_grid,1)):
     e = event_log[event_log['ts'] <= time].groupby(by=['contagion']).count()['ts']
     for tag in e.index:
         d[tag] = e[tag]/num_users
+    with open(directory + 'data-overview/fractions/day_' + str(count), 'w', encoding='utf-8') as f:
+        f.write('\n'.join('%s %s' % x for x in sorted(d.items(), key=lambda kv: kv[1], reverse=True)))
+    # open(directory + 'data-overview/fractions/day_' + str(count), 'w', encoding='utf-8').close()
+    # for pair in sorted(d.items(), key=lambda kv: kv[1], reverse=True):
+    #     with open(directory + 'data-overview/fractions/day_' + str(count), 'a', encoding='utf-8') as f:
+    #         f.write(str(pair[0])+','+str(pair[1])+'\n')
     plt.figure(figsize=(12,6))
     plt.hist(d.values(), range=(0, 1), bins=50)
     plt.axvline(np.mean(list(d.values())), color='r', linestyle='dashed', linewidth=1)

f95dfa16f9989429fe146b41bf275365f2642d47
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-07-03 13:57:03 +0200

    model - estimation_algorithms - contagion_correlation - working version

--- experiments/estimation_algorithms/contagion_correlation.py
+++ experiments/estimation_algorithms/contagion_correlation.py
@@ -16,5 +16,19 @@ sets_to_compare = [x.strip() for x in sets_to_compare]
 
 directory = '/nfs/maciej/mcdoi/louvain/'
 
+
 def compare(path):
-    pass
+    with open(path + '/data_obj.pickle', 'rb') as f:
+        d = pickle.load(f)
+    cc = ContagionCorrelation()
+    cc.estimate(d)
+    with open(path + '/contagion.pickle', 'rb') as f:
+        cc_present = pickle.load(f)
+    if not np.all(cc.matrix==cc_present):
+        print(path)
+        print(cc.matrix[cc.matrix!=cc_present])
+        print(cc_present[cc.matrix!=cc_present])
+
+if __name__ == '__main__':
+    for path in sets_to_compare:
+        compare(path)

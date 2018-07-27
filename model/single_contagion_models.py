eac43359b6042cf546333e1412a77eb7fa6dfe4a
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-07-27 11:23:30 +0200

    model - single_contagion_models - SingleContagionDynamicThresholdModel - whole predict copied from MultiContagion...

--- model/single_contagion_models.py
+++ model/single_contagion_models.py
@@ -34,8 +34,8 @@ class SingleContagionDynamicThresholdModel(MultiContagionDynamicThresholdModel):
 
     def __init__(self):
 
-        super(SingleContagionDynamicThresholdModel, self).__init__()
-        # self.adjacency = Adjacency()
+        # super(SingleContagionDynamicThresholdModel, self).__init__()
+        self.adjacency = Adjacency()
         self.thresholds = ThresholdSingleContagion()
         self.contagion_correlation = None
 
@@ -85,6 +85,87 @@ class SingleContagionDynamicThresholdModel(MultiContagionDynamicThresholdModel):
 
     def __activation_matrix(self, influence_matrix):
         return influence_matrix
+
+    def predict(self, num_iterations: int) -> Results:
+        # TODO "method" rst
+        # TODO replace num_activation by proper test
+        """
+
+        Parameters
+        ----------
+        num_iterations : int
+            Discrete number of prediction iteration steps to perform by :method:predict method
+
+        Returns
+        -------
+        Results
+            Object containing results from all predication iterations.
+
+        """
+        global num_activations
+        num_activations = 0
+        result = Results()
+        self.adjacency.transposed()
+        for l in range(num_iterations):
+            result.add_result(self.__single_iteration())
+        # print(num_activations)
+        return result
+
+    def __single_iteration(self) -> SingleIterResult:
+        # TODO SingleIterResult -> new special class
+        influence_matrix = self.__influence_matrix()
+        activation_matrix = self.__activation_matrix(influence_matrix)
+        self.__activation_procedure(activation_matrix)
+        return self.state_matrix_
+
+    def __activation_procedure(self, activation_matrix):
+        # TODO delete num_activations
+        global num_activations
+        activation_candidates = self.__find_activation_candidates(activation_matrix)
+        for user in self.__users_above_threshold(activation_candidates):
+            contagions_above_threshold = self.__contagions_above_threshold(activation_candidates,
+                                                      user)  # contagions for user in which threshold has been exceeded
+            active_contagions = self.active_contagions(user)  # contagions in which user is already active
+            contagions_above_threshold_not_active = self.__contagions_above_threshold_not_active(active_contagions,
+                                                                                                 contagions_above_threshold)  # delete active_contagions from contagions_above_threshold
+            if self.__check_negative_contagion_correlation(contagions_above_threshold_not_active):  # check weather
+                # candidates are not negatively correlated
+                self.__activation(contagions_above_threshold_not_active, user)
+                self.__increase_activity_index(user)
+                num_activations += 1
+                self.__update_threshold(user)
+
+    def __update_threshold(self, user):
+        # TODO assign vector in one line
+        for contagion in range(self.state_matrix_.num_contagions):  # temporary solution
+            self.thresholds.matrix[user][contagion] = 1 - math.pow(
+                1 - self.thresholds.initial_matrix[user][contagion],
+                self.activity_index_vector_[user] + 1)  # aktualizacja thety
+
+    def __increase_activity_index(self, user):
+        self.activity_index_vector_[user] += 1  # Y[user]+=1 #zwiekszenie licznika aktywacji uzytkownika user
+
+    def __activation(self, contagions_above_threshold_not_active, user):
+        self.state_matrix_.matrix[user][
+            contagions_above_threshold_not_active] = True  # aktywacja uzytkownika user w tagach z listy contagions_above_threshold
+
+    def __contagions_above_threshold_not_active(self, active_contagions, contagions_above_threshold):
+        return np.setdiff1d(contagions_above_threshold, active_contagions)
+
+    def active_contagions(self, user):
+        return np.where(self.state_matrix_.matrix[user][:])[0]
+
+    def __contagions_above_threshold(self, activation_candidates, user):
+        return np.where(activation_candidates[user, :])[0]
+
+    def __users_above_threshold(self, activation_candidates):
+        return np.unique(np.where(activation_candidates[:, :])[0])
+
+    def __find_activation_candidates(self, activation_matrix):
+        return np.greater_equal(activation_matrix, self.thresholds.matrix)
+
+    def __influence_matrix(self):
+        return self.adjacency.matrix_transposed_.dot(self.state_matrix_.matrix)
 
     def __check_negative_contagion_correlation(self, contagions_above_threshold_not_active):
         return contagions_above_threshold_not_active.size > 0

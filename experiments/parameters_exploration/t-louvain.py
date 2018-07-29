684b4b73045008d2f32a1b54c935b22b0cbc86d1
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-07-29 19:42:12 +0200

    t-louvain - 24h batch added

--- experiments/parameters_exploration/t-louvain.py
+++ experiments/parameters_exploration/t-louvain.py
@@ -42,7 +42,7 @@ def ParallelExecutor(use_bar='tqdm', **joblib_args):
         return tmp
     return aprun
 
-aprun = ParallelExecutor(n_jobs=18)
+aprun = ParallelExecutor(n_jobs=12)
 
 directory = '/nfs/maciej/mcdoi/dynamic-linear-threshold/'
 
@@ -50,7 +50,7 @@ with open(directory+'estimated_t+predict', 'r', encoding='utf-8') as file:
     estimated = file.readlines()
 estimated = [x.strip() for x in estimated]
 
-batch_sizes = [604800]# [43200, 86400, 604800] # (1h), 12h, 24h, 7d
+batch_sizes = [86400,604800]# [43200, 86400, 604800] # (1h), 12h, 24h, 7d
 batch_sizes.reverse()
 
 # with open(directory + 'sets_to_omit', 'r', encoding='utf-8') as sets_to_omit:

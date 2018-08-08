d0dfb996276cfec94304ba82c45a5b3f4c9b8dc5
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-08-08 13:07:47 +0200

    t-louvain - assignig cc matrix recovered

--- experiments/parameters_exploration/t-louvain.py
+++ experiments/parameters_exploration/t-louvain.py
@@ -78,6 +78,8 @@ def estimate_t_and_predict(path_dataset_history, batch_type, batch_sizes, num_pr
     if flag:
         edges = pd.read_csv(os.path.dirname(path_dataset_history) + '/edges', header = None)
         event_log = pd.read_csv(path_dataset_history + '/event_log', header=None)
+        with open(path_dataset_history + '/contagion.pickle', 'rb') as file:
+            cc = pickle.load(file)
         with open(path_dataset_history + '/adjacency.pickle', 'rb') as file:
             a = pickle.load(file)
         for batch_size in batch_sizes:
@@ -91,6 +93,7 @@ def estimate_t_and_predict(path_dataset_history, batch_type, batch_sizes, num_pr
                     with open(path_dataset_history + '/data_obj.pickle', 'wb') as f:
                         pickle.dump(d, f)
                 m = MCDOI()
+                m.assign_contagions_correlation_matrix(cc)
                 m.assign_adjacency_matrix(a)
                 m.fit_only_thresholds_states(d, batch_type = batch_type, batch_size = batch_size)
                 new_path_dataset_history = path_dataset_history.split('/')

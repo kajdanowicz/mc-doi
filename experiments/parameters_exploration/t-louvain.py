5671316033f917a74fdee06db0d880d04be96420
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-08-14 20:38:22 +0200

    t-louvain - better multithread

--- experiments/parameters_exploration/t-louvain.py
+++ experiments/parameters_exploration/t-louvain.py
@@ -14,6 +14,12 @@ with open(sets_to_estimate_file, 'r', encoding='utf-8') as sets_to_estimate:
     sets_to_estimate = sets_to_estimate.readlines()
 sets_to_estimate = [x.strip() for x in sets_to_estimate]
 
+tmp =[]
+for s in sets_to_estimate:
+    tmp.append(s+'/time' + '/size_604800')
+
+sets_to_estimate = tmp
+
 from joblib import Parallel, delayed
 import time
 def text_progessbar(seq, total=None):
@@ -41,7 +47,7 @@ def ParallelExecutor(use_bar='tqdm', **joblib_args):
         return tmp
     return aprun
 
-aprun = ParallelExecutor(n_jobs=8)
+aprun = ParallelExecutor(n_jobs=4)
 
 directory = '/nfs/maciej/mcdoi/correlated-linear-threshold/'
 
@@ -49,7 +55,9 @@ with open(directory+'estimated_t+predict', 'r', encoding='utf-8') as file:
     estimated = file.readlines()
 estimated = [x.strip() for x in estimated]
 
-batch_sizes = [86400,604800]# [43200, 86400, 604800] # (1h), 12h, 24h, 7d
+estimated.append('/nfs/maciej/mcdoi/louvain/louvain_48_2234/history_3/time/size_604800')
+
+batch_sizes = [604800]# [43200, 86400, 604800] # (1h), 12h, 24h, 7d
 batch_sizes.reverse()
 
 # with open(directory + 'sets_to_omit', 'r', encoding='utf-8') as sets_to_omit:
@@ -71,44 +79,38 @@ def save_results(result: Results, dir, num_predictions):
 
 
 def estimate_t_and_predict(path_dataset_history, batch_type, batch_sizes, num_predictions, estimated):
-    flag = False
-    for batch_size in batch_sizes:
-        if path_dataset_history+ '/' + batch_type + '/size_' + str(batch_size) not in estimated:
-            flag = True
-    if flag:
-        edges = pd.read_csv(os.path.dirname(path_dataset_history) + '/edges', header = None)
-        event_log = pd.read_csv(path_dataset_history + '/event_log', header=None)
-        with open(path_dataset_history + '/contagion.pickle', 'rb') as file:
-            cc = pickle.load(file)
-        with open(path_dataset_history + '/adjacency.pickle', 'rb') as file:
-            a = pickle.load(file)
-        for batch_size in batch_sizes:
-            if path_dataset_history+ '/' + batch_type + '/size_' + str(batch_size) not in estimated:
-                if os.path.isfile(path_dataset_history + '/data_obj.pickle'):
-                    with open(path_dataset_history + '/data_obj.pickle', 'rb') as f:
-                        d = pickle.load(f)
-                else:
-                    d = Data()
-                    d.load_data_data_frame(event_log, edges)
-                    with open(path_dataset_history + '/data_obj.pickle', 'wb') as f:
-                        pickle.dump(d, f)
-                m = MCDOI()
-                m.assign_contagions_correlation_matrix(cc)
-                m.assign_adjacency_matrix(a)
-                m.fit_only_thresholds_states(d, batch_type = batch_type, batch_size = batch_size)
-                new_path_dataset_history = path_dataset_history.split('/')
-                new_path_dataset_history[4] = 'correlated-linear-threshold'
-                new_path_dataset_history = '/'+os.path.join(*new_path_dataset_history)
-                file_name = new_path_dataset_history + '/' + batch_type + '/size_' + str(batch_size) + '/threshold.pickle'
-                os.makedirs(os.path.dirname(file_name), exist_ok=True)
-                with open(file_name, 'wb') as threshold_file:
-                    pickle.dump(m.thresholds.matrix, threshold_file)
-                result = m.predict(num_predictions)
-                save_results(result, new_path_dataset_history + '/' + batch_type + '/size_' + str(batch_size), num_predictions)
-                with open(directory+'estimated_t+predict', 'a+', encoding='utf-8') as handle:
-                    handle.write(path_dataset_history + '/' + batch_type + '/size_' + str(batch_size) + '\n')
-                with open(directory + 'predicted_7days', 'a+', encoding='utf-8') as handle:
-                    handle.write(path_dataset_history + '/' + batch_type + '/size_' + str(batch_size) + '\n')
+    path_dataset_h = os.path.dirname(os.path.dirname(path_dataset_history))
+    edges = pd.read_csv(os.path.dirname(path_dataset_h) + '/edges', header = None)
+    event_log = pd.read_csv(path_dataset_h + '/event_log', header=None)
+    with open(path_dataset_h + '/contagion.pickle', 'rb') as file:
+        cc = pickle.load(file)
+    with open(path_dataset_h + '/adjacency.pickle', 'rb') as file:
+        a = pickle.load(file)
+    if os.path.isfile(path_dataset_h + '/data_obj.pickle'):
+        with open(path_dataset_h + '/data_obj.pickle', 'rb') as f:
+            d = pickle.load(f)
+    else:
+        d = Data()
+        d.load_data_data_frame(event_log, edges)
+        with open(path_dataset_h + '/data_obj.pickle', 'wb') as f:
+            pickle.dump(d, f)
+    m = MCDOI()
+    m.assign_contagions_correlation_matrix(cc)
+    m.assign_adjacency_matrix(a)
+    m.fit_only_thresholds_states(d, batch_type = batch_type, batch_size = 604800)
+    new_path_dataset_history = path_dataset_h.split('/')
+    new_path_dataset_history[4] = 'correlated-linear-threshold'
+    new_path_dataset_history = '/'+os.path.join(*new_path_dataset_history)
+    file_name = new_path_dataset_history + '/' + batch_type + '/size_' + str(604800) + '/threshold.pickle'
+    os.makedirs(os.path.dirname(file_name), exist_ok=True)
+    with open(file_name, 'wb') as threshold_file:
+        pickle.dump(m.thresholds.matrix, threshold_file)
+    result = m.predict(num_predictions)
+    save_results(result, new_path_dataset_history + '/' + batch_type + '/size_' + str(604800), num_predictions)
+    with open(directory+'estimated_t+predict', 'a+', encoding='utf-8') as handle:
+        handle.write(path_dataset_history + '\n')
+    with open(directory + 'predicted_7days', 'a+', encoding='utf-8') as handle:
+        handle.write(path_dataset_history + '\n')
 
 
 # def make_dataset_history_paths():
@@ -118,8 +120,12 @@ def estimate_t_and_predict(path_dataset_history, batch_type, batch_sizes, num_pr
 #             paths.append(directory+dat+'/history_'+str(history_length))
 #     return paths
 
+def diff(first, second):
+    second = set(second)
+    return [item for item in first if item not in second]
+
 if __name__ == '__main__':
-    aprun(bar='txt')(delayed(estimate_t_and_predict)(dat, 'time', batch_sizes, 7, estimated) for dat in sets_to_estimate)
+    aprun(bar='txt')(delayed(estimate_t_and_predict)(dat, 'time', batch_sizes, 7, estimated) for dat in diff(sets_to_estimate,estimated))
 
 
 

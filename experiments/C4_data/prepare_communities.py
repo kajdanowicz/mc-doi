dbd1b5caf32e28ffa43ac8c73ba3f430842b718e
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-08-19 17:37:58 -0400

    prepare_communities for C4 data added

---
+++ experiments/C4_data/prepare_communities.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import networkx as nx
+import community
+import pickle
+
+with open('/nfs/maciej/mcdoi/c4/active_follower_real_csv', 'r', encoding='utf-8') as f:
+    df = pd.read_csv(f, header=None, names=['user1','user2'])
+
+graph = nx.from_pandas_dataframe(df, 'user1', 'user2')
+
+partition_louvain=community.best_partition(graph)
+
+partition_louvian_groups = [set() for index in range(max(partition_louvain.values())+1)]
+for key,value in partition_louvain.items():
+    partition_louvian_groups[value].add(key)
+
+with open('/nfs/maciej/mcdoi/c4/louvain_communities.pickle', 'wb') as handle:
+    pickle.dump(partition_louvian_groups, handle)
+
+
+

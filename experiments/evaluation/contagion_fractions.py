9700b567b242b2fe7b00b49113f186cb605bc86f
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-08-22 00:03:23 -0400

    contagion_fractions - for all sets

--- experiments/evaluation/contagion_fractions.py
+++ experiments/evaluation/contagion_fractions.py
@@ -10,43 +10,67 @@ from data.data import Data
 from collections import defaultdict
 import functools
 import itertools
+from tqdm import tqdm
 
-path = '/nfs/maciej/mcdoi/louvain/louvain_55_123/history_20/time/size_604800'
-history=20
+sets_to_evaluate_file = list(sys.argv)[1]
+with open(sets_to_evaluate_file, 'r', encoding='utf-8') as sets_to_evaluate:
+    sets_to_evaluate = sets_to_evaluate.readlines()
+sets_to_evaluate = [x.strip() for x in sets_to_evaluate]
 
 start_time = 1332565200
 end_time = 1335416399
 duration_24h_in_sec = 60*60*24
 time_grid = np.arange(start_time+duration_24h_in_sec,end_time+duration_24h_in_sec,duration_24h_in_sec)
-iter_length = 86400
 
-with open(os.path.dirname(os.path.dirname(os.path.dirname(path)))+'/edges', 'r', encoding='utf-8') as f:
-    edges = pd.read_csv(f, header=None, names=[Data.user_1, Data.user_2])
+directory = '/nfs/maciej/mcdoi/louvain/'
 
-user_dict = defaultdict(functools.partial(next, itertools.count()))
-edges[Data.user_1] = edges[Data.user_1].map(user_dict)
-edges[Data.user_2] = edges[Data.user_2].map(user_dict)
+evaluated = set()
+for batch_size in [3600, 43200, 86400, 604800]:
+    with open(directory + 'frequencies/contagion_fractions_'+str(batch_size), 'r', encoding='utf-8') as file:
+        e = file.readlines()
+    evaluated.update([x.strip() for x in e])
 
-with open(os.path.dirname(os.path.dirname(os.path.dirname(path)))+'/event_log', 'r', encoding='utf-8') as f:
-    whole_event_log = pd.read_csv(f, header=None, names=[Data.time_stamp, Data.user, Data.contagion])
-whole_event_log.user = whole_event_log.user.map(user_dict)
+def diff(first, second):
+    second = set(second)
+    return [item for item in first if item not in second]
 
-with open(os.path.dirname(os.path.dirname(path))+'/data_obj.pickle', 'rb') as f:
-    d=pickle.load(f)
+def evaluate(path, iter_length):
+    history = int(path.split('/')[6].split('_')[1])
+    batch_size = int(path.split('/')[8].split('_')[1])
+    with open(os.path.dirname(os.path.dirname(os.path.dirname(path)))+'/edges', 'r', encoding='utf-8') as f:
+        edges = pd.read_csv(f, header=None, names=[Data.user_1, Data.user_2])
 
-# print(d.event_log.head())
+    user_dict = defaultdict(functools.partial(next, itertools.count()))
+    edges[Data.user_1] = edges[Data.user_1].map(user_dict)
+    edges[Data.user_2] = edges[Data.user_2].map(user_dict)
 
-results = []
-for i in range(0, 7):
-    with open(path + '/result_' + str(i) + '.pickle', 'rb') as result:
-        results.append(pickle.load(result))
+    with open(os.path.dirname(os.path.dirname(os.path.dirname(path)))+'/event_log', 'r', encoding='utf-8') as f:
+        whole_event_log = pd.read_csv(f, header=None, names=[Data.time_stamp, Data.user, Data.contagion])
+    whole_event_log.user = whole_event_log.user.map(user_dict)
 
-for i in range(1,min(7,33-history)+1):
-    open(path + '/contagion_fractions_' + str(i - 1), 'w', encoding='utf-8').close()
-    e = whole_event_log[whole_event_log['ts'] <= time_grid[history - 1] + i * iter_length].drop_duplicates(subset=['contagion', 'user'], keep='first')
-    e = e.groupby(by=['user']).count()['ts']
-    for user in range(d.num_users):
-        with open(path + '/contagion_fractions_' + str(i - 1), 'a', encoding='utf-8') as file:
-            file.write(str(user) + ',' + str(e.get(user,0) / d.num_contagions) + ',' + str(np.sum(results[i - 1], axis=1)[user] / d.num_contagions) + '\n')
+    with open(os.path.dirname(os.path.dirname(path))+'/data_obj.pickle', 'rb') as f:
+        d=pickle.load(f)
+
+    # print(d.event_log.head())
+
+    results = []
+    for i in range(0, 7):
+        with open(path + '/result_' + str(i) + '.pickle', 'rb') as result:
+            results.append(pickle.load(result))
+
+    for i in range(1,min(7,33-history)+1):
+        open(path + '/contagion_fractions_' + str(i - 1), 'w', encoding='utf-8').close()
+        e = whole_event_log[whole_event_log['ts'] <= time_grid[history - 1] + i * iter_length].drop_duplicates(subset=['contagion', 'user'], keep='first')
+        e = e.groupby(by=['user']).count()['ts']
+        for user in range(d.num_users):
+            with open(path + '/contagion_fractions_' + str(i - 1), 'a', encoding='utf-8') as file:
+                file.write(str(user) + ',' + str(e.get(user,0) / d.num_contagions) + ',' + str(np.sum(results[i - 1], axis=1)[user] / d.num_contagions) + '\n')
+        with open(directory + 'frequencies/contagion_fractions_' + str(batch_size), 'a', encoding='utf-8') as file:
+            file.write(path + '/contagion_fractions_' + str(i - 1) + '\n')
+
+if __name__ == '__main__':
+    paths = diff(sets_to_evaluate,evaluated)
+    for path in tqdm(paths):
+        evaluate(path,86400)
 
 

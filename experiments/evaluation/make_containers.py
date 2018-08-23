7755d96419429c0e1dff1ead7a039daa1ad7977d
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-08-23 11:07:34 -0400

    make_containers - init

---
+++ experiments/evaluation/make_containers.py
@@ -0,0 +1,12 @@
+import os
+
+models = ['correlated-linear-threshold', 'dynamic-linear-threshold', 'linear-threshold','linear-threshold-model','linear-threshold-model-14all','linear-threshold-random-dynamic','linear-threshold-random-dynamic-14all']
+batch_sizes = ['3600', '43200','86400','604800']
+measures = ['contagion_fscore_','contagion_fscore_diff_','fscore_','fscore_diff_','fractions_diff_', 'frequencies_','contagion_jaccard_diff_','hamming_','jaccard_','jaccard_diff_']
+
+for model in models:
+    for batch_size in batch_sizes:
+        directory = '/nfs/maciej/mcdoi/'+model+'/'
+        for measure in measures:
+            os.makedirs(directory+'frequencies/', exist_ok=True)
+            open(directory+'frequencies/'+measure+batch_size, 'w', encoding='utf-8').close()

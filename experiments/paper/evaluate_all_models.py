08e5bcaa8764df08592a7c8ca6f7e9fca5004be3
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-09-14 10:44:38 +0200

    script to evaluate all models

---
+++ experiments/paper/evaluate_all_models.py
@@ -0,0 +1,9 @@
+import os
+script_path = '/home/maciek/pyCharmProjects/mc-doi/experiments/paper/evaluation/evaluate_measures.py'
+home_path = '/nfs/maciej/mcdoi/paper/'
+
+models = ['correlated-linear-dynamic-threshold','correlated-linear-threshold','linear-dynamic-threshold','linear-threshold','linear-threshold-random','linear-threshold-random-dynamic','linear-threshold-random-dynamic-single-theta','linear-threshold-random-single-theta']
+
+for model in models:
+    os.system('sudo python3 '+script_path+' '+home_path+model+'/predicted'+' '+model)
+    # print('sudo python3 '+script_path+' '+home_path+model+'/predicted'+' '+model)

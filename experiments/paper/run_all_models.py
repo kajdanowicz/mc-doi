7485b3430c550171cbde5f706dc9f3e491d224f0
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-09-14 10:35:50 +0200

    script to run all models

---
+++ experiments/paper/run_all_models.py
@@ -0,0 +1,9 @@
+import os
+home_path = '/home/maciek/pyCharmProjects/mc-doi/experiments/paper/'
+path_to_datasets = '/nfs/maciej/mcdoi/paper/estimated_cc+a'
+
+models = ['correlated-linear-dynamic-threshold','correlated-linear-threshold','linear-dynamic-threshold','linear-threshold','linear-threshold-random','linear-threshold-random-dynamic','linear-threshold-random-dynamic-single-theta','linear-threshold-random-single-theta']
+
+for model in models:
+    os.system('sudo python3 '+home_path+model+'/estimate_thresholds_and_predict.py '+path_to_datasets+' '+model)
+    # print('sudo python3 '+home_path+model+'/estimate_thresholds_and_predict.py '+path_to_datasets+' '+model)

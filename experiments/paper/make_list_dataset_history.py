e539f1ba8b387f71b038bc2684992c407a23835b
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-09-14 10:13:31 +0200

    universal evaluation script for paper experiments

--- experiments/paper/make_list_dataset_history.py
+++ experiments/paper/make_list_dataset_history.py
@@ -1,9 +1,9 @@
-# datasets = ['99_5','55_123','101_7','48_2234']
+datasets = ['99_5','55_123','101_7','48_2234', '46_720','53_502','31_462','54_222','80_12']
 
 file_directory_name = '/nfs/maciej/mcdoi/paper/estimated_cc+a'
 
-datasets = ['99_5']
-histories = [10]
+# datasets = ['99_5']
+histories = [10,20]
 open(file_directory_name, 'w', encoding='utf-8').close()
 for s in datasets:
     for history in histories:

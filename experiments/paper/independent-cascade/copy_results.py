a0419e632661247b995d9ac59272fc136e8a423a
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-09-22 11:43:00 +0200

    copy_results for independent-cascade model imporved

--- experiments/paper/independent-cascade/copy_results.py
+++ experiments/paper/independent-cascade/copy_results.py
@@ -1,11 +1,17 @@
 import os
 sets_to_copy_path = '/nfs/maciej/mcdoi/paper/estimated_cc+a'
 
+with open('/nfs/maciej/mcdoi/paper/independent-cascade/predicted','r',encoding='utf-8') as f:
+    predicted = f.readlines()
+
 with open(sets_to_copy_path, 'r', encoding='utf-8') as f:
     sets = f.readlines()
 
 new_sets = []
 for path in sets:
+    if path not in predicted:
+        with open('/nfs/maciej/mcdoi/paper/independent-cascade/predicted', 'a', encoding='utf-8') as f:
+            f.write(path)
     new_path = path.split('/')
     new_path[4] = 'independent-cascade'
     new_path = '/' + os.path.join(*new_path)

c801a35f54b1b810d42385886316755cb7d626ac
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-11-12 14:40:38 +0100

    'linear-dynamic-threshold','linear-threshold' added to evaluation

--- experiments/paper/evaluate_all_models.py
+++ experiments/paper/evaluate_all_models.py
@@ -3,8 +3,8 @@ script_path = '/home/maciek/pyCharmProjects/mc-doi/experiments/paper/evaluation/
 home_path = '/nfs/maciej/mcdoi/paper/'
 
 # models = ['correlated-linear-dynamic-threshold','correlated-linear-threshold','linear-dynamic-threshold','linear-threshold','linear-threshold-random','linear-threshold-random-dynamic','linear-threshold-random-dynamic-single-theta','linear-threshold-random-single-theta','correlated-linear-dynamic-threshold-continuous']
-models = ['correlated-linear-dynamic-threshold','correlated-linear-threshold','linear-threshold-random','linear-threshold-random-dynamic','linear-threshold-random-dynamic-single-theta','linear-threshold-random-single-theta']
+models = ['linear-dynamic-threshold','linear-threshold','correlated-linear-dynamic-threshold','correlated-linear-threshold','linear-threshold-random','linear-threshold-random-dynamic','linear-threshold-random-dynamic-single-theta','linear-threshold-random-single-theta']
 
-for model in reversed(models):
+for model in models:
     os.system('sudo python3 '+script_path+' '+home_path+model+'/predicted'+' '+model)
     # print('sudo python3 '+script_path+' '+home_path+model+'/predicted'+' '+model)

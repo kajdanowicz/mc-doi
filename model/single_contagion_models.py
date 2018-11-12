895657174eeaedc7d90297680df836f4423eea83
Maciej Falkiewicz <falkiewicz.maciej@gmail.com>
2018-11-12 12:51:34 +0100

    linear-threshold - deepcopy added in prediction

--- model/single_contagion_models.py
+++ model/single_contagion_models.py
@@ -270,7 +270,7 @@ class SingleContagionLinearThresholdModel(MultiContagionDynamicLinearThresholdMo
             influence_matrix = self.__influence_matrix()
             activation_matrix = self.__activation_matrix(influence_matrix)
             self.__activation_procedure(activation_matrix)
-            return self.state_matrix_
+            return deepcopy(self.state_matrix_)
 
         def __activation_procedure(self, activation_matrix):
             # TODO delete num_activations
